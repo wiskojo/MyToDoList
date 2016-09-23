fce48f8f64be4c494c3672b2f6d158811b435e84
Wis Kojohnjaratkul <Wis Kojohnjaratkul>
2016-09-22 21:55:17 -0700

    Added the basic ability to add tasks

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
 $(function()
 {
-  
+  $("ul#tasks").on("click", "button.sub-btn", function()
+  {
+    var $displays = $(this).parent().children(".display");
+    $displays.each(function()
+    {
+      $(this).text($(this).prev("input").val());
+    });
+
+    $(this).parent().addClass("edit");
+  });
+
+  $("button#add-task").click(function()
+  {
+    var task = "<li class=\"task\">" +
+                "<input value=\"Task Description\"></input>" +
+                "<span class=\"display\"></span>" +
+                "<input type=\"date\"></input>" +
+                "<span class=\"display\"></span>" +
+                "<button class=\"sub-btn\">Submit</button>" +
+               "</li>"
+
+    $("ul#tasks").append(task);
+  });
+
 }); // End Main
